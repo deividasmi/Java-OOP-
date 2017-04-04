619b344e5fe04776a7728b1e4fce49d9d161957b
juoz <dmz357@gmail.com>
2017-04-04 10:51:14 +0300

    class clothes

---
+++ eShop/src/eshop/Clothes.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eshop;
+
+/**
+ *
+ * @author deividas
+ */
+public class Clothes {
+    
+    private String clout;
+    private String colour;
+    private int size;
+    
+}
