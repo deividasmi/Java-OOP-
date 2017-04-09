756eb1b7bff6544c358b35fc5f2da405ceb43ccb
WildsG <dulka.arturas@gmail.com>
2017-04-09 20:22:20 +0300

    Update timeflow, added text panel to represent items

--- eShop/src/eshop/TimeManager.java
+++ eShop/src/eshop/TimeManager.java
@@ -13,33 +13,19 @@ package eshop;
 
 public class TimeManager {
    //public class variables
-    public int StartMonth;
-    public int StartDay;
-    public int StartHour;
-    
-    public int EndMonth;
-    public int EndDay;
-    public int EndHour;
-    //checking if time was inputted correctly
-    public Integer checkTimeInput(String StartDate,String EndDate){
-        if(StartDate.substring(2, 3).equals(".") && StartDate.substring(5, 6).equals("."))
-        {
-            if(EndDate.substring(2, 3).equals(".") && EndDate.substring(5, 6).equals("."))
-                return 1;
-        }
-        return null;
-    }
+    //setting default date
+    public int StartYear = 17;
+    public int StartMonth = 1;
+    public int StartDay = 1;
     //setting start date
-    public void setStartDate(int month, int day, int hour){
+    public void setStartDate(int year, int month, int day){
+        StartYear = year;
         StartMonth = month;
         StartDay = day;
-        StartHour = hour;
     }
-    //setting end date
-    public void setEndDate(int month, int day, int hour){
-        EndMonth = month;
-        EndDay = day;
-        EndHour = hour;
+    public int getStartYear()
+    {
+        return StartYear;
     }
     
     public int getStartMonth()
@@ -52,30 +38,6 @@ public class TimeManager {
         return StartDay;
     }
     
-    public int getStartHour()
-    {
-        return StartHour;
-    }
     
-    public int getEndMonth()
-    {
-        return EndMonth;
-    }
-    
-    public int getEndDay()
-    {
-        return EndDay;
-    }
-    
-    public int getEndHour()
-    {
-        return EndHour;
-    }
-    //outputting time for testing purposes
-    public void OutputTime()
-    {
-        System.out.println(StartMonth+" "+StartDay+" "+ StartHour);
-         System.out.println(EndMonth+" "+EndDay+" "+ EndHour);
-    }
    
 }
