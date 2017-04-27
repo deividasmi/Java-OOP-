e9ea44869432321b88c36f09efe31ff264c55c23
WildsG <dulka.arturas@gmail.com>
2017-04-27 15:07:56 +0300

    dasdas

--- eShop/src/eshop/Clothes.java
+++ eShop/src/eshop/Clothes.java
@@ -42,16 +42,9 @@ public class Clothes extends Item{
     @Override
     public String toString(){
         //return id + " " + item + " " + brand + " " + model + " " + type + " " + size + " " + amount + " " + price + " " + stockUp;
-<<<<<<< HEAD
         return String.format("%10s %10s %10s %10s %4s %.2f",item,brand,model,type,size,price);
-=======
-        return String.format("%10s %10s %10s %10s %4s %4d   %.2f",item,brand,model,type,size,amount,price);
     }
-    public String toCartString()
-    {
-        return String.format("%5s %10s %10s %10s %10s %4s %4d   %.2f", id,item,brand,model,type,size,cartAmount,price);
->>>>>>> dc51015ab992f9de2caf6b19c8ef30fdb99581c0
-    }
+
    public String toCartString()
    {
         return String.format("%10s %10s %10s %10s %4s %4d   %.2f",item,brand,model,type,size,cartAmount,price);
